2f148f6cc57413c9ba677e45b3ecb5a3a49e297e
nourFarid <118819227+nourFarid@users.noreply.github.com>
2023-09-14 03:58:26 +0300

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+   { path: 'moviesdetails/:id', component: MovieDetailsComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
